a8282c27574af35af0fa21039fb7bed12ae27856
SaranshPrasad <saranshprasad08@gmail.com>
2026-05-25 14:06:18 +0530

    vercel added

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req,res) => {
-    res.json({msg:"Hellow from StackX"});
+    res.json({msg:"Hellow from StacX"});
 });
 
 
